11ff4c5b04a4f892ce7b76c3b1a4a25fcab0970b
That Lisa <thatlisajones@gmail.com>
2019-04-28 22:19:20 -0600

    Is it repaired?

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
-document.getElementById(date).innerhtml=moment().format('MMMM Do YYYY')
-document.getElementById(time).innerhtml=moment().format('h:mm:ss a')
+document.getElementById('date').innerText=moment().format('MMMM Do YYYY')
+document.getElementById('time').innerText=moment().format('h:mm:ss a')
 
 window.setInterval(getTime, 500);
 
@@ -8,20 +8,20 @@ function getTime() {
 }
 
 gifAddition = (arr) =>{
-  for (const i = 0; i < arr.length; i++) {
+  for (var i = 0; i < arr.length; i++) {
     console.log("moment test: ", moment(arr[i].import_datetime).format('MMMM Do YYYY'));
 
-    const cardHolder = $("div").addclass("col s12 m4")
-    const card =  $("div").addclass("card col-content")
-    const cardImage =  $("div").addclass("card-image waves-effect waves-block waves-light")
-    const image =  $("img").addclass("activator").attr("src", arr[i].images.fixed_height.url)
-    const content = $("div").addclass("card-content")
-    const title = $("span").addclass("card-title activator grey-html html-darken-4").html(arr[i].title)
-    const moreIcon = $("i").addclass("material-icons right").html("more_vert")
-    const reveal = $("div").addclass("card-reveal")
-    const close = $("span").addclass("card-title grey-html html-darken-4").html(arr[i].title)
-    const closeIcon = $("i").addclass("material-icons right").html("close")
-    const info = $("p").addclass("revealhtml").html(`This gif is rated ${arr[i].rating} and was added to Giphy on ${moment(arr[i].import_datetime).format('MMMM Do YYYY')}`)
+    let cardHolder = $("<div>").addClass("col s12 m4")
+    let card =  $("<div>").addClass("card col-content")
+    let cardImage =  $("<div>").addClass("card-image waves-effect waves-block waves-light")
+    let image =  $("<img>").addClass("activator").attr("src", arr[i].images.fixed_height.url)
+    let content = $("<div>").addClass("card-content")
+    let title = $("<span>").addClass("card-title activator grey-html html-darken-4").html(arr[i].title)
+    let moreIcon = $("<i>").addClass("material-icons right").html("more_vert")
+    let reveal = $("<div>").addClass("card-reveal")
+    let close = $("<span>").addClass("card-title grey-html html-darken-4").html(arr[i].title)
+    let closeIcon = $("<i>").addClass("material-icons right").html("close")
+    let info = $("<p>").addClass("revealText").text(`This gif is rated ${arr[i].rating} and was added to Giphy on ${moment(arr[i].import_datetime).format('MMMM Do YYYY')}`)
 
     cardImage.append(image)
     card.append(cardImage)
@@ -34,13 +34,13 @@ gifAddition = (arr) =>{
     card.append(reveal)
     cardHolder.append(card)
 
-    $("gifHolder").append(cardHolder)
+    $("#gifHolder").append(cardHolder)
   }
 }
 
-fetch('https://api.giphy.com/v1/gifs/trending?api_key=Oks2BSCHq8adUf0GBIeqg3AWBng0GkCZ&limit=5')
+fetch('https://api.giphy.com/v1/gifs/trending?api_key=Oks2BSCHq8adUf0GBIeqg3AWBng0GkCZ&limit=3')
   .then(response => {
-      return response.json;
+      return response.json();
   })
   .then(json => {
     gifAddition(json.data)
